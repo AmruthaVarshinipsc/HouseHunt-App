adbbaeb62b64cc3c0bb8fdc43b40c9f7fb7835a4
AmruthaVarshinipsc <22MH1A05K3@acoe.edu.in>
2025-06-25 16:02:11 +0530

    Initial project upload

---
+++ frontend/src/modules/common/Home.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Button } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import p1 from '../../images/p1.jpg'
+import p2 from '../../images/p2.jpg'
+import p3 from '../../images/p3.jpg'
+import p9 from '../../images/p9.jpg'
+import p5 from '../../images/p5.jpg'
+import p6 from '../../images/p6.jpg'
+import AllPropertiesCards from '../user/AllPropertiesCards';
+
+const Home = () => {
+   const [index, setIndex] = useState(0);
+
+   const handleSelect = (selectedIndex) => {
+      setIndex(selectedIndex);
+   };
+   return (
+      <>
+         <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+               <Navbar.Brand><h2>HouseHunt</h2></Navbar.Brand>
+               <Navbar.Toggle aria-controls="navbarScroll" />
+               <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                     className="me-auto my-2 my-lg-0"
+                     style={{ maxHeight: '100px' }}
+                     navbarScroll
+                  >
+                  </Nav>
+                  <Nav>
+                     <Link to={'/'}>Home</Link>
+                     <Link to={'/login'}>Sign In</Link>
+                     <Link to={'/register'}>Sign Up</Link>
+                  </Nav>
+
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
+
+
+         <div className='home-body'>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+               <Carousel.Item>
+                  <img
+                     src={p5}
+                     alt="First slide"
+                  />
+               </Carousel.Item>
+               <Carousel.Item>
+                  <img
+                     src={p6}
+                     alt="Second slide"
+                  />
+               </Carousel.Item>
+               <Carousel.Item>
+                  <img
+                     src={p3}
+                     alt="Third slide"
+                  />
+               </Carousel.Item>
+               <Carousel.Item>
+                  <img
+                     src={p9}
+                     alt="Fourth slide"
+                  />
+               </Carousel.Item>
+            </Carousel>
+         </div>
+
+
+         <div className='property-content'>
+            <div className='text-center'>
+               <h1 className='m-1 p-5'>All Properties that may you look for</h1>
+               <p style={{fontSize: 15, fontWeight: 800}}>Want to post your Property? <Link to={'/register'}><Button variant='outline-info'>Register as Owner</Button></Link></p>
+            </div>
+
+            <Container>
+               <AllPropertiesCards />
+            </Container>
+         </div>
+      </>
+   )
+}
+
+export default Home
